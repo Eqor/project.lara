9b89f6d06e108d009033ce964535b4a8753b0fcc
Eqor <bishop1996@mail.ru>
2022-03-26 21:17:35 +0300

    Validate create request CategoryController.php

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Blog\Admin;
 
 
 use App\Http\Requests\BlogCategoryUpdateRequest;
+use App\Http\Requests\BlogCategoryCreateRequest;
 use App\Models\BlogCategory;
-use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 
 
 class CategoryController extends BaseController
@@ -30,7 +32,11 @@ class CategoryController extends BaseController
      */
     public function create()
     {
-        dd(__METHOD__);
+        $item = new BlogCategory();
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.category.edit',
+            compact('item','categoryList'));
     }
 
     /**
@@ -39,9 +45,28 @@ class CategoryController extends BaseController
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BlogCategoryCreateRequest $request)
     {
-        dd(__METHOD__);
+        $data = $request->input();
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+
+
+        $item = new BlogCategory($data);
+        $item->save();
+
+        if ($item) {
+            return redirect()->route('blog.admin.categories.edit', $item->id)
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => "ошибка сохранения"])
+                ->withInput();
+        }
+
+
     }
 
     /**
