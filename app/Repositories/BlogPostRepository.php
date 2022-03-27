8875cec87c254c723f45cf4a025d6d71e1fc8593
Eqor <bishop1996@mail.ru>
2022-03-27 20:44:36 +0300

    Create View BlogPosts

---
+++ app/Repositories/BlogPostRepository.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\BlogPost as Model;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+
+/**
+ * Class BlogCategoryRepository
+ * @package App\Repositories
+ */
+class BlogPostRepository extends CoreRepository
+{
+    /**
+     * @return string
+     */
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    /**
+     * Получить модель для редактирования в админке.
+     * @param int $id
+     * @return  Model
+     */
+    public function getEdit(int $id)
+    {
+        return $this->startConditions()->find($id);
+    }
+
+    /**
+     * @param int|null $perPage
+     * @return LengthAwarePaginator
+     */
+    public  function getAllWithPaginate( int $perPage = null)
+    {
+        $colums = [
+            'id',
+            'title',
+            'slug',
+            'is_published',
+            'published_at',
+            'user_id',
+            'category_id'
+        ];
+
+        $result = $this->startConditions()
+            ->select($colums)
+            ->orderBy('id', 'DESC')
+            ->paginate(25);
+
+        return $result;
+
+    }
+
+    /**
+     * Получите список категорий для вывода в выпадающем списке
+     * @return Collection
+     */
+    public function getForComboBox()
+    {
+        //return $this->startConditions()->all();
+
+        $colums = implode(', ',[
+            'id',
+            'CONCAT (id, ". ", title) AS id_title',
+        ]);
+
+        $result = $this
+            ->startConditions()
+            ->selectRaw($colums)
+            ->toBase()
+            ->get();
+
+
+        return $result;
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,14 @@ Route::group($groupData, function (){
     $methods = ['index','create','store','edit','update',];
     Route::resource('categories', 'CategoryController')->only($methods)
         ->names('blog.admin.categories');
+
+    // BlogPost
+    Route::resource('posts', 'PostController')
+        ->except(['show'])
+        ->names('blog.admin.posts');
 });
+
+
 
 
 
